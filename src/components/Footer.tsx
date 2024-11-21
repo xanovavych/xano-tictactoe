0a6257139aae2016221d9fda237370957e64e5e4
xanovavych <afterlife0614@gmail.com>
2024-11-21 16:05:02 +0800

    second

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <p>Tic tac toe - playable in diffirent tabs - with local storage</p>
+      <p>Tic tac toe Game</p>
+      <p>Play in diff tabs : with local storage</p>
     </footer>
   );
 }
